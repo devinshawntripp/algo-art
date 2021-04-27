496652d5db8610e694e2f73c4e7d3148e2e11663
Devin Tripp <devinshawntripp@gmail.com>
2021-04-26 23:00:59 -0500

    changes

--- frontend/src/Pages/Sentiment.jsx
+++ frontend/src/Pages/Sentiment.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {FormControl, Form, Button, Container } from 'react-bootstrap'
 
 
 
 
 export default function Sentiment() {
+
+    const searchBox = {
+        padding: "2.0rem",
+        display: "flex",
+        // width: "60.0rem",
+        flexDirection: "row",
+
+    }
     return (
-        <div>some sentiment</div>
+        <Container style={{justifyContent: "center", alignItems: "center", marginLeft: "auto", marginRight: "auto"}}>
+            <Form style={searchBox}>
+                <Form.Control style={{marginRight: "2.0rem"}} type="search" placeholder="Search" />
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
     )
 }
