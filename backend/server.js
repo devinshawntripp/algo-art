d1296c1a27d9dffd9036d34ff239276ed4f9b589
Devin Tripp <devinshawntripp@gmail.com>
2021-04-29 21:16:16 -0500

    server.js fixes

--- backend/server.js
+++ backend/server.js
@@ -44,9 +44,9 @@ const io = socketio(server, options)
 require('./routers/twitter-router.js')(app, io)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (request, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 } else {
   port = 8174;
